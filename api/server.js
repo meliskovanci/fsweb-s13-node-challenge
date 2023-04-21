7aae6ecd713f1182d215ef5affa5f6e04fedce1e
Melis Kovancı <meliskovanci@hotmail.com>
2023-04-21 12:36:15 +0300

    server

--- api/server.js
+++ api/server.js
@@ -1,9 +1,23 @@
 const express = require('express');
+
+const projectsRouter = require("./projects/projects-router");
+const actionsRouter = require("./actions/actions-router");
+
 const server = express();
 
 // Sunucunuzu yapılandırın
 // Eylem routerınızı /api/actions/actions-router.js içinde oluşturun
 // Proje roterlarınızı /api/projects/projects-router.js içinde oluşturun
 // Bu dosyanın içinde `server.listen()` YAPMAYIN!
+
+
+
+server.use("/api/projects", projectsRouter);
+server.use("/api/actions", actionsRouter);
+
+server.get("/", (req,res)=> {
+    res.send("it is working");
+})
+
 
 module.exports = server;
